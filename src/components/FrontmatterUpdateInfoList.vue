b239f926978cda58e57423cc39e1f6af6f85376d
smori <shinichiro.mori.1983@gmail.com>
2022-08-12 23:09:09 +0900

    Display with similar HTML structure

--- src/components/FrontmatterUpdateInfoList.vue
+++ src/components/FrontmatterUpdateInfoList.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <div v-for="page in updates">
-      <router-link :to="page.path">{{ page.title }}</router-link>
-      <new-badge :threshold="newThreshold" :date="page.dateLast"></new-badge>
-      <ul>
-        <li v-for="record in page.records">
-          <span>{{ record.date }}</span>
-          <ul v-if="record.description.length > 0">
-            <li v-for="line in record.description">{{ line }}</li>
-          </ul>
+    <div v-for="page in updates" class="frontmatter-update-info-page">
+      <div class="link">
+        <router-link :to="page.path">{{ page.title }}</router-link>
+        <new-badge :threshold="newThreshold" :date="page.dateLast"></new-badge>
+      </div>
+      <ul class="records">
+        <li v-for="record in page.records" class="record">
+          <div class="date">{{ record.date }}</div>
+          <div class="description">
+            <ul v-if="record.description.length > 0">
+              <li v-for="line in record.description">{{ line }}</li>
+            </ul>
+          </div>
         </li>
       </ul>
     </div>
@@ -55,4 +59,31 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.frontmatter-update-info-page {
+  margin-bottom 1.0rem
+
+  .link {
+    margin-bottom 0.5rem
+  }
+
+  .records {
+    margin 0
+
+    .record {
+      .date {
+        line-height 1.5rem
+      }
+
+      .description {
+        ul {
+          margin 0
+
+          li {
+            line-height 1.5rem
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- src/components/FrontmatterUpdateInfoTable.vue
+++ src/components/FrontmatterUpdateInfoTable.vue
@@ -5,12 +5,14 @@
         <router-link :to="page.path">{{ page.title }}</router-link>
         <new-badge :threshold="newThreshold" :date="page.dateLast"></new-badge>
       </div>
-      <div v-for="record in page.records" class="record">
-        <div class="date">{{ record.date }}</div>
-        <div class="description">
-          <ul v-if="record.description.length > 0">
-            <li v-for="line in record.description">{{ line }}</li>
-          </ul>
+      <div class="records">
+        <div v-for="record in page.records" class="record">
+          <div class="date">{{ record.date }}</div>
+          <div class="description">
+            <ul v-if="record.description.length > 0">
+              <li v-for="line in record.description">{{ line }}</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -64,25 +66,27 @@ export default {
     margin-bottom 0.5rem
   }
 
-  .record {
-    display flex
-    margin-bottom -1px
-    border-top 1px solid lightgray
-    border-bottom 1px solid lightgray
+  .records {
+    .record {
+      display flex
+      margin-bottom -1px
+      border-top 1px solid lightgray
+      border-bottom 1px solid lightgray
 
-    .date {
-      width 20%
-      line-height 2rem
-    }
+      .date {
+        width 20%
+        line-height 2rem
+      }
 
-    .description {
-      width 80%
+      .description {
+        width 80%
 
-      ul {
-        margin 0
+        ul {
+          margin 0
 
-        li {
-          line-height 2rem
+          li {
+            line-height 2rem
+          }
         }
       }
     }
