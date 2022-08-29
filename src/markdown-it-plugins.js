bf6a06b8a540855b4843718cf792e702f7f99086
smori <shinichiro.mori.1983@gmail.com>
2022-08-30 01:05:09 +0900

    Inject component tag after <h1>

--- src/markdown-it-plugins.js
+++ src/markdown-it-plugins.js
@@ -1,5 +1,29 @@
 module.exports = (md) => {
   md.core.ruler.push('vuepress_plugin_frontmatter_update_info', (state) => {
-    console.log('called');
+    if (state.tokens.length < 3) {
+      return;
+    }
+
+    const token1 = state.tokens[0];
+    const token2 = state.tokens[1];
+    const token3 = state.tokens[2];
+
+    if (!(token1.type === 'heading_open' && token1.tag === 'h1')) {
+      return;
+    }
+
+    if (!(token2.type === 'inline')) {
+      return;
+    }
+
+    if (!(token3.type === 'heading_close' && token3.tag === 'h1')) {
+      return;
+    }
+
+    const token = new state.Token('html_block', '', 0);
+    token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
+    token.block = true;
+
+    state.tokens.splice(3, 0, token);
   });
 };
