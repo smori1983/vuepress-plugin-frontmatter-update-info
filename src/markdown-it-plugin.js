843c81a79d2df7d8affcfe8e6c88051dbcea5bec
smori <shinichiro.mori.1983@gmail.com>
2022-09-03 02:29:16 +0900

    Initial implement as block rule

--- src/markdown-it-plugin.js
+++ src/markdown-it-plugin.js
@@ -34,4 +34,36 @@ module.exports = (md) => {
 
     state.tokens.splice(3, 0, token);
   });
+
+  md.block.ruler.before('table', 'vuepress_plugin_frontmatter_update_info_tag', (state, startLine, endLine, silent) => {
+    if (state.tokens.length < 3) {
+      return false;
+    }
+
+    const tokenLength = state.tokens.length;
+    const token1 = state.tokens[tokenLength - 3];
+    const token2 = state.tokens[tokenLength - 2];
+    const token3 = state.tokens[tokenLength - 1];
+
+    if (!(token1.type === 'paragraph_open')) {
+      return false;
+    }
+
+    if (!(token2.type === 'inline' && token2.content === marker)) {
+      return false;
+    }
+
+    if (!(token3.type === 'paragraph_close')) {
+      return false;
+    }
+
+    const token = new state.Token('html_block', '', 0);
+    token.map = token1.map;
+    token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
+    token.block = true;
+
+    state.tokens.splice(-3, 3, token);
+
+    return true;
+  });
 };
