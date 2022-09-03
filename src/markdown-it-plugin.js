7db0d889cbe5b34c3802b05136a160f44f7e61c6
smori <shinichiro.mori.1983@gmail.com>
2022-09-03 13:39:31 +0900

    Extract code to function

--- src/markdown-it-plugin.js
+++ src/markdown-it-plugin.js
@@ -1,56 +1,59 @@
 const marker = '[[update_info]]';
 
+const coreRule = (state) => {
+  if (state.src.includes(marker)) {
+    return;
+  }
+
+  if (state.tokens.length < 3) {
+    return;
+  }
+
+  const token1 = state.tokens[0];
+  const token2 = state.tokens[1];
+  const token3 = state.tokens[2];
+
+  const targetHeadingLevels = ['h1', 'h2'];
+
+  if (!(token1.type === 'heading_open' && targetHeadingLevels.includes(token1.tag))) {
+    return;
+  }
+
+  if (!(token2.type === 'inline')) {
+    return;
+  }
+
+  if (!(token3.type === 'heading_close' && targetHeadingLevels.includes(token3.tag))) {
+    return;
+  }
+
+  const token = new state.Token('html_block', '', 0);
+  token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
+  token.block = true;
+
+  state.tokens.splice(3, 0, token);
+};
+
+const blockRule = (state, startLine, endLine, silent) => {
+  const lineText = state.src.slice(state.bMarks[startLine], state.eMarks[startLine]);
+
+  if (lineText !== marker) {
+    return false;
+  }
+
+  state.line = startLine + 1;
+
+  const token = new state.Token('html_block', '', 0);
+  token.map = [startLine, state.line];
+  token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
+  token.block = true;
+
+  state.tokens.push(token);
+
+  return true;
+};
+
 module.exports = (md) => {
-  md.core.ruler.push('vuepress_plugin_frontmatter_update_info', (state) => {
-    if (state.src.includes(marker)) {
-      return;
-    }
-
-    if (state.tokens.length < 3) {
-      return;
-    }
-
-    const token1 = state.tokens[0];
-    const token2 = state.tokens[1];
-    const token3 = state.tokens[2];
-
-    const targetHeadingLevels = ['h1', 'h2'];
-
-    if (!(token1.type === 'heading_open' && targetHeadingLevels.includes(token1.tag))) {
-      return;
-    }
-
-    if (!(token2.type === 'inline')) {
-      return;
-    }
-
-    if (!(token3.type === 'heading_close' && targetHeadingLevels.includes(token3.tag))) {
-      return;
-    }
-
-    const token = new state.Token('html_block', '', 0);
-    token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
-    token.block = true;
-
-    state.tokens.splice(3, 0, token);
-  });
-
-  md.block.ruler.before('paragraph', 'vuepress_plugin_frontmatter_update_info_tag', (state, startLine, endLine, silent) => {
-    const lineText = state.src.slice(state.bMarks[startLine], state.eMarks[startLine]);
-
-    if (lineText !== marker) {
-      return false;
-    }
-
-    state.line = startLine + 1;
-
-    const token = new state.Token('html_block', '', 0);
-    token.map = [startLine, state.line];
-    token.content = '<PluginFrontmatterUpdateInfoPageEmbed/>';
-    token.block = true;
-
-    state.tokens.push(token);
-
-    return true;
-  });
+  md.core.ruler.push('vuepress_plugin_frontmatter_update_info', coreRule);
+  md.block.ruler.before('paragraph', 'vuepress_plugin_frontmatter_update_info_tag', blockRule);
 };
