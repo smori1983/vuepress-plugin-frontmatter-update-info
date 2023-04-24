0fad265ba6a4a1f151ac649f8359c8b0ecea6793
smori <shinichiro.mori.1983@gmail.com>
2023-04-24 22:46:33 +0900

    [test] generation.js

--- test/generation-util/generation.js
+++ test/generation-util/generation.js
@@ -4,49 +4,51 @@ const it = require('mocha').it;
 const Generation = require('../../src/generation-util/generation');
 
 describe('Generation', () => {
-  const generationData = [
-    {
-      path: '/page_1.html',
-      title: 'Page 1',
-      records: [
-        {
-          date: '2023/01/01',
-          description: [
-            'Page 1 added.',
-          ],
-        },
-      ],
-    },
-  ];
+  describe('Num of pages: 1', () => {
+    const generationData = [
+      {
+        path: '/page_1.html',
+        title: 'Page 1',
+        records: [
+          {
+            date: '2023/01/01',
+            description: [
+              'Page 1 added.',
+            ],
+          },
+        ],
+      },
+    ];
 
-  it('getPaths() returns collected page paths', () => {
-    const generation = new Generation(generationData);
+    it('getPaths() returns collected page paths', () => {
+      const generation = new Generation(generationData);
 
-    const paths = generation.getPaths();
+      const paths = generation.getPaths();
 
-    assert.deepStrictEqual(paths, ['/page_1.html']);
-  });
+      assert.deepStrictEqual(paths, ['/page_1.html']);
+    });
 
-  it('getPage() returns null if the path not exists', () => {
-    const generation = new Generation(generationData);
+    it('getPage() returns null if the path not exists', () => {
+      const generation = new Generation(generationData);
 
-    const page = generation.getPage('/foo.html');
+      const page = generation.getPage('/foo.html');
 
-    assert.deepStrictEqual(page, null);
-  });
+      assert.deepStrictEqual(page, null);
+    });
 
-  it('getPage() returns page object if the path exists', () => {
-    const generation = new Generation(generationData);
+    it('getPage() returns page object if the path exists', () => {
+      const generation = new Generation(generationData);
 
-    const page = generation.getPage('/page_1.html');
+      const page = generation.getPage('/page_1.html');
 
-    assert.deepStrictEqual(page.getPath(), '/page_1.html');
-    assert.deepStrictEqual(page.getTitle(), 'Page 1');
+      assert.deepStrictEqual(page.getPath(), '/page_1.html');
+      assert.deepStrictEqual(page.getTitle(), 'Page 1');
 
-    const records = page.getRecords();
+      const records = page.getRecords();
 
-    assert.deepStrictEqual(records.length, 1);
-    assert.deepStrictEqual(records[0].date, '2023/01/01');
-    assert.deepStrictEqual(records[0].description, ['Page 1 added.']);
+      assert.deepStrictEqual(records.length, 1);
+      assert.deepStrictEqual(records[0].date, '2023/01/01');
+      assert.deepStrictEqual(records[0].description, ['Page 1 added.']);
+    });
   });
 });
