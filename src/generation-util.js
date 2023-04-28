ff3a08c981f3d72327a9ec16d8ab1ce95b16d8a1
smori <shinichiro.mori.1983@gmail.com>
2023-04-28 23:39:16 +0900

    generation-util.js

---
+++ src/generation-util.js
@@ -0,0 +1,3 @@
+module.exports.Generation = require('./generation-util/generation');
+module.exports.DiffStyleDate = require('./generation-util/diff-style-date');
+module.exports.DiffStyleDateDescription = require('./generation-util/diff-style-date-description');
